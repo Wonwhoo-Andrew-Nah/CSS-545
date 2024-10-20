f76095d2c0a971ba20799f60cfd99dde7784f18c
Andrew Nah <auspiciouswho47@gmail.com>
2024-10-20 13:29:27 -0700

    feat: init firebase

--- pawframe/lib/main.dart
+++ pawframe/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
