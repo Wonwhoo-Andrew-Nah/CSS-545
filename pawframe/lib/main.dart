72165ba29804e20a4576e7a5454df354473ac89f
Andrew Nah <auspiciouswho47@gmail.com>
2024-10-20 15:21:11 -0700

    feat: shared_preference

--- pawframe/lib/main.dart
+++ pawframe/lib/main.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,15 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -67,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _savePreferences() async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString('animalType', _animalType!);
-    prefs.setDouble('animalAge', _animalAge!);
+    prefs.setDouble('animalAge', _animalAge);
     prefs.setString('zipCode', _zipController.text);
   }
 
@@ -145,22 +132,45 @@ class AnimalSwipeScreen extends StatefulWidget {
 }
 
 class _AnimalSwipeScreenState extends State<AnimalSwipeScreen> {
-  // Mock data
-  final List<Map<String, dynamic>> _animals = [
-    {'name': 'Buddy', 'type': 'Dog', 'isAdopted': false},
-    {'name': 'Mittens', 'type': 'Cat', 'isAdopted': false},
-    {'name': 'Max', 'type': 'Dog', 'isAdopted': false},
+  List<Map<String, dynamic>> _animals = [
+    {
+      'name': 'Buddy',
+      'type': 'Dog',
+      'imageUrl': 'https://petharbor.com/get_image.asp?RES=Detail&ID=A712125&LOCATION=KING',
+      'isAdopted': false,
+    },
+    {
+      'name': 'Mittens',
+      'type': 'Cat',
+      'imageUrl': 'https://petharbor.com/get_image.asp?RES=Detail&ID=A716708&LOCATION=KING',
+      'isAdopted': false,
+    },
+    {
+      'name': 'Max',
+      'type': 'Dog',
+      'imageUrl': 'https://petharbor.com/get_image.asp?RES=Detail&ID=A717530&LOCATION=KING',
+      'isAdopted': false,
+    },
   ];
 
-  // TODO: fix parameter into String ID
-  Future<void> _adoptAnimal(String animalName) async {
-    await FirebaseFirestore.instance
-        .collection('adoptions')
-        .doc(animalName)
-        .set({
-      'adopted': true,
-      'timestamp': FieldValue.serverTimestamp(),
+  @override
+  void initState() {
+    super.initState();
+    _loadAnimalData();
+  }
+
+  Future<void> _loadAnimalData() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      for (var animal in _animals) {
+        animal['isAdopted'] = prefs.getBool(animal['name']) ?? false;
+      }
     });
+  }
+
+  Future<void> _adoptAnimal(String animalName) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool(animalName, true);
 
     setState(() {
       _animals.firstWhere(
@@ -200,6 +210,7 @@ class _AnimalSwipeScreenState extends State<AnimalSwipeScreen> {
               );
             },
             child: ListTile(
+              leading: Image.network(animal['imageUrl']), // Display the image
               title: Text(animal['name']),
               subtitle: Text('Type: ${animal['type']}'),
               trailing: animal['isAdopted']
